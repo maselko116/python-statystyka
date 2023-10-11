646bd0f2d01ce307e33dcd88c6c4ddb9c191bff1
vdi-terminal <vdi-terminal@EDU-WSB.local>
2023-10-11 11:11:52 +0200

    data bases

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+import pandas as pd
+# sieci = pd.read_csv('dane/sieci.csv', sep=',')
+# kategorie = pd.read_csv('dane/kategorie.csv', sep=',')
+drogi = pd.read_csv('dane/drogi.csv', sep="\t")
+# stacje = pd.read_csv('dane/stacje.csv', sep=',')
+
+# print(sieci.index)
+# print(kategorie.index)
+# print(drogi.index)
+# print(stacje.index)
+
+#print(drogi.head())
+#print(drogi['tid_kategorii'])
+#print(drogi.value_counts())
+typy_drog = drogi['id_kategorii']
+print(typy_drog)
+
+print(drogi.value_counts().head(10))
