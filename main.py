bf036883255bf78277d4b3f9d794d4e8d64110c5
Jakub Masłowski <kiku116m@gmail.com>
2023-10-18 11:12:38 +0200

    prawie gotowe es

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 # sieci = pd.read_csv('dane/sieci.csv', sep=',')
 # kategorie = pd.read_csv('dane/kategorie.csv', sep=',')
 drogi = pd.read_csv('dane/drogi.csv', sep="\t")
+kategorie = pd.read_csv('dane/kategorie.csv', sep="\t")
+stacje = pd.read_csv('dane/stacje.csv', sep="\t")
+
 # stacje = pd.read_csv('dane/stacje.csv', sep=',')
 
-# print(sieci.index)
-# print(kategorie.index)
-# print(drogi.index)
-# print(stacje.index)
 
-#print(drogi.head())
-#print(drogi['tid_kategorii'])
-#print(drogi.value_counts())
+drogi_z_kategoriami = pd.merge(drogi,kategorie, on= 'id_kategorii')
+
+
+
 typy_drog = drogi['id_kategorii']
-print(typy_drog)
 
-print(drogi.value_counts().head(10))
+
+
+#print(drogi.sort_values(by='dlugosc', ascending=False))
+
+grupowane = drogi_z_kategoriami.groupby('kategoria')
+wyniki = grupowane.agg({'dlugosc': 'sum', 'id_drogi': 'count'}).reset_index()
+wyniki = wyniki.rename(columns={'dlugosc': 'suma_dlugosci', 'id_drogi': 'ilosc_drog'})
+
+#print(wyniki)
+
+plt.figure(figsize=(8, 6))
+plt.bar(wyniki['kategoria'], wyniki['suma_dlugosci'])
+plt.xlabel('Kategoria')
+plt.ylabel('Suma Długości Dróg')
+plt.title('Suma Długości Dróg według Kategorii')
+plt.show()
+
+# Tworzenie wykresu słupkowego dla ilości dróg
+plt.figure(figsize=(8, 6))
+plt.bar(wyniki['kategoria'], wyniki['ilosc_drog'])
+plt.xlabel('Kategoria')
+plt.ylabel('Ilość Dróg')
+plt.title('Ilość Dróg według Kategorii')
+plt.show()
+
+stacje_z_drogami = pd.merge(drogi,stacje, on= 'id_drogi')
+# grupowane2 = stacje_z_drogami.groupby('id_drogi')
+# wyniki2 = grupowane2.agg({'id_drogi': 'count'}).reset_index()
+# print(wyniki2)
+
+ilosc_stacji_na_drodze = stacje_z_drogami.groupby('id_drogi')['id_stacji'].count().reset_index()
+ilosc_stacji_na_drodze = ilosc_stacji_na_drodze.rename(columns={'id_stacji': 'ilosc_stacji'})
+
+print(ilosc_stacji_na_drodze.head(5).sort_values(by='ilosc_stacji', ascending=False))
+
+
