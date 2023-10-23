4ea17119b9f2cb86f06cfed21743c50a95e76678
Jakub Masłowski <kiku116m@gmail.com>
2023-10-23 19:53:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ grupowane = drogi_z_kategoriami.groupby('kategoria')
 wyniki = grupowane.agg({'dlugosc': 'sum', 'id_drogi': 'count'}).reset_index()
 wyniki = wyniki.rename(columns={'dlugosc': 'suma_dlugosci', 'id_drogi': 'ilosc_drog'})
 
-#print(wyniki)
+#WYKRES 1 suma długości dróg
 
 plt.figure(figsize=(8, 6))
 plt.bar(wyniki['kategoria'], wyniki['suma_dlugosci'])
@@ -32,7 +32,7 @@ plt.ylabel('Suma Długości Dróg')
 plt.title('Suma Długości Dróg według Kategorii')
 plt.show()
 
-# Tworzenie wykresu słupkowego dla ilości dróg
+# WYKRES 2 dla ilości dróg
 plt.figure(figsize=(8, 6))
 plt.bar(wyniki['kategoria'], wyniki['ilosc_drog'])
 plt.xlabel('Kategoria')
@@ -46,8 +46,33 @@ stacje_z_drogami = pd.merge(drogi,stacje, on= 'id_drogi')
 # print(wyniki2)
 
 ilosc_stacji_na_drodze = stacje_z_drogami.groupby('id_drogi')['id_stacji'].count().reset_index()
+
 ilosc_stacji_na_drodze = ilosc_stacji_na_drodze.rename(columns={'id_stacji': 'ilosc_stacji'})
 
 print(ilosc_stacji_na_drodze.head(5).sort_values(by='ilosc_stacji', ascending=False))
 
+polaczone_dane = pd.merge(ilosc_stacji_na_drodze, drogi, on='id_drogi')
+top_5_drogi = polaczone_dane.sort_values(by='ilosc_stacji', ascending=False).head(5)
 
+# WYKRES 3 ilość stacji na drogach
+plt.figure(figsize=(10, 6))
+plt.bar(top_5_drogi['nazwa'], top_5_drogi['ilosc_stacji'])
+plt.xlabel('nazwa')
+plt.ylabel('Ilość Stacji')
+plt.title('Top 5 Drog według Ilości Stacji')
+plt.xticks(rotation=45)  
+plt.show()
+
+
+print(wyniki)
+
+najdluzsze_drogi = drogi_z_kategoriami.loc[drogi_z_kategoriami.groupby('kategoria')['dlugosc'].idxmax()]
+
+# WYKRES 4
+plt.figure(figsize=(10, 6))
+plt.bar(najdluzsze_drogi['kategoria'], najdluzsze_drogi['dlugosc'])
+plt.xlabel('Kategoria Drogowa')
+plt.ylabel('Długość Drogi w km')
+plt.title('Najdłuższe Drogi w Każdej Kategorii')
+plt.xticks(najdluzsze_drogi['kategoria'])
+plt.show()
